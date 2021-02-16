408ca8a041c605db989731e97d7c1bc0a90a8b2d
davidvaughan86 <david.vaughan86@gmail.com>
2021-02-16 11:13:54 -0500

    fixes stripe

--- loginController.js
+++ loginController.js
@@ -11,11 +11,11 @@ const loginVerify = async (req, res) => {
     console.log('Username: ', username)
     console.log('Password: ', password)
 
-    //Store username in session
+    
     req.session.username = username;
     
     
-    // Check to see if they exist in the database. If so redirect to quiz selection page
+    
     const user = await users.findOne({
         where: {
             email: username
@@ -25,9 +25,9 @@ const loginVerify = async (req, res) => {
     console.log("USER", user)
 
     if(user) {
-        //Compare req.body.password with user.hash
+       
         const isValid = bcrypt.compareSync(password, user.password);
-        //If password matches with hash
+       
         console.log(isValid)
         if(isValid){
             req.session.user_id = user.id;
